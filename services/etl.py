a0bfecd753fbae12b2a6f4c53de6052d25a9c327
rplain1 <ryan.plain1@gmail.com>
2025-02-25 09:26:08 -0700

    so ruff so clean

--- services/etl.py
+++ services/etl.py
@@ -8,15 +8,13 @@ from datetime import datetime
 import os
 
 CONFIG = {
-    "DATABASE": {
-        'SQLite' : 'data/base.base.sqlite',
-        'DuckDB' : 'data/luna.luna.duckdb'
-    }
+    "DATABASE": {"SQLite": "data/base.base.sqlite", "DuckDB": "data/luna.luna.duckdb"}
 }
+
 
 class ETL:
     def __init__(self, config_file):
-        self.config = config_file #self.load_config(config_file)
+        self.config = config_file  # self.load_config(config_file)
         self.raw_data = None
         self.transformed_data = None
         self.setup_logging()
@@ -27,24 +25,32 @@ class ETL:
         return config
 
     def setup_logging(self):
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.basicConfig(
+            level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+        )
 
     def extract(self):
         """Extract data from SQLite database."""
         try:
-            sqlite_connection_string = "sqlite://" + self.config['DATABASE']['SQLite']['PATH']
-            table = self.config['DATABASE']['SQLite']['TABLE']
-            self.raw_data = pl.read_database_uri(f"SELECT * FROM {table}", sqlite_connection_string)
+            sqlite_connection_string = (
+                "sqlite://" + self.config["DATABASE"]["SQLite"]["PATH"]
+            )
+            table = self.config["DATABASE"]["SQLite"]["TABLE"]
+            self.raw_data = pl.read_database_uri(
+                f"SELECT * FROM {table}", sqlite_connection_string
+            )
             logging.info("Data extraction complete.")
         except Exception as e:
-            logging.error(f"Error during extraction of DB {sqlite_connection_string}: {e}")
+            logging.error(
+                f"Error during extraction of DB {sqlite_connection_string}: {e}"
+            )
 
     def transform(self):
         """Transform the raw data."""
         try:
             # Example transformation: Add a timestamp column
             self.transformed_data = self.raw_data.with_columns(
-                RUN_AT = datetime.now()
+                RUN_AT=datetime.now()
             )  # Placeholder for more complex transformations
             logging.info("Data transformation complete.")
         except Exception as e:
@@ -53,9 +59,9 @@ class ETL:
     def load(self):
         """Load transformed data into DuckDB."""
         try:
-            duckdb_conn = duckdb.connect(self.config['DATABASE']['DuckDB']['PATH'])
-            schema = self.config['DATABASE']['DuckDB']['SCHEMA']
-            table = self.config['DATABASE']['DuckDB']['TABLE']
+            duckdb_conn = duckdb.connect(self.config["DATABASE"]["DuckDB"]["PATH"])
+            schema = self.config["DATABASE"]["DuckDB"]["SCHEMA"]
+            table = self.config["DATABASE"]["DuckDB"]["TABLE"]
 
             duckdb_conn.execute(f"CREATE SCHEMA IF NOT EXISTS {schema};")
             duckdb_conn.execute(f"DROP TABLE IF EXISTS {schema}.{table}")
@@ -78,18 +84,14 @@ class ETL:
         self.load()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     config_file = {
         "DATABASE": {
-            "SQLite": {
-                "PATH": "data/pbp_db.sqlite",
-                "TABLE": "nflfastR_pbp"
-            },
+            "SQLite": {"PATH": "data/pbp_db.sqlite", "TABLE": "nflfastR_pbp"},
             "DuckDB": {
                 "PATH": "data/luna.duckdb",
                 "SCHEMA": "BASE",
-                "TABLE": 'NFLFASTR_PBP'
+                "TABLE": "NFLFASTR_PBP",
             },
         }
     }
