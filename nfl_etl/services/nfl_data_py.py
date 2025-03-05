a6c9ff7755e95a1c4d985cbc1f64be4e2bbaa332
rplain1 <ryan.plain1@gmail.com>
2025-03-05 08:18:20 -0700

    build years and ftn funcs developed

--- nfl_etl/services/nfl_data_py.py
+++ nfl_etl/services/nfl_data_py.py
@@ -1,27 +1,75 @@
-NFLVERSE_DATA_URL = "https://github.com/nflverse/nflverse-data/releases/download"
-LATEST_YEAR = 2024
+from nfl_etl.services.globals import NFLVERSE_DATA_URL, LATEST_YEAR
+import logging
 
 
-def build_years_list(years, latest_year=LATEST_YEAR):
-    if isinstance(years, int):
+def is_list_of_strings(lst: list) -> bool:
+    """Check if list contains any string values"""
+    return any(isinstance(item, str) for item in lst)
+
+
+def build_years_list(
+    years: int | list[int] | str | list[str],
+    latest_year: int = LATEST_YEAR,
+    min_year: int | None = None,
+) -> list:
+    """
+    Create a list of years that can be a single year, or a range of years,
+    depending on the arguments passed. This will take a string, int, or list and
+    return the appropriate range.
+
+    Args:
+        years: (int | str | list[int] | list[str]) years to build the list of
+        latest_year: (int, optional) Max year of available data
+        min_year: (int | None, optional) Min year of available data
+
+    Returns:
+        list: list of years for a desired range
+    """
+
+    # check if single value was passed instead of iterable
+    if isinstance(years, int) or isinstance(years, str):
         years = [years]
 
+    # convert string years
+    if is_list_of_strings(years):
+        try:
+            years = [int(x) for x in years]
+        except Exception as e:
+            raise ValueError(f"An error occured casting to int {e}")
+
+    # cant have future data
     if max(years) > LATEST_YEAR:
         raise ValueError(f"Year must be less than {latest_year}")
+
+    # setup list of years to provide
     years = sorted(set(years))
     if years[-1] - years[0] > 0:
         years = [x for x in range(min(years), max(years) + 1)]
 
+    # cant have data that never existed
+    if min_year and min(years) < min_year:
+        logging.warning(f"Data first available in {min_year}, removing prior years")
+        years = [x for x in years if x >= min_year]
+
     return years
 
 
-def ftn_charting_file_list(years, url=NFLVERSE_DATA_URL):
-    assert min(years) >= 2022, "FTN data began in 2022"
-    years = build_years_list(years)
+def ftn_charting_file_list(
+    years: int | list[int] | str | list[str], url: str = NFLVERSE_DATA_URL
+) -> list:
+    """
+    Build a list of urls for ftn_charting nflverse data.
+
+    Args:
+        years: (int | str | list[int] | list[str]) years to build the list of
+        url: (str, optional) url path of ftn data
+
+    Returns:
+        list: list of urls for parquet files
+    """
+    years = build_years_list(years, min_year=2022)
+    if all(x < 2022 for x in years):
+        raise ValueError("FTN data must start on or after 2022")
     paths = [f"{url}/ftn_charting/ftn_charting_{x}.parquet" for x in years]
 
     return paths
-
-
-if __name__ == "__main__":
-    print(build_years_list(2023))
--- tests/test_nfl_data_py.py
+++ tests/test_nfl_data_py.py
@@ -1,26 +1,71 @@
 import pytest
-from nfl_etl.services.nfl_data_py import build_years_list
+from nfl_etl.services.nfl_data_py import build_years_list, ftn_charting_file_list
 
 
 @pytest.mark.parametrize(
     "input_year",
-    [3100],  # Test with invalid years like 0 and -1
+    [3100, "$312"],
 )
 def test_build_years_invalid(input_year):
+    """Test that invalid years raise an error"""
     with pytest.raises(ValueError):  # Expect ValueError to be raised
         build_years_list(input_year)
 
 
 @pytest.mark.parametrize(
-    "input_year, expected",  # Ensure the parameters are comma-separated
+    "input_year, expected",
     [
         (2023, [2023]),
         ([2023, 2024], [2023, 2024]),
         ([2022, 2024], [2022, 2023, 2024]),
         ([2022, 2020], [2020, 2021, 2022]),
         ([2022, 2022], [2022]),
+        (["2022"], [2022]),
+        ("2023", [2023]),
+        (["2023", 2020], [2020, 2021, 2022, 2023]),
     ],
 )
 def test_build_years(input_year, expected):
+    """
+    Test all the comibinations of supported inputs
+    """
     test = build_years_list(input_year)
     assert test == expected
+
+
+@pytest.mark.parametrize(
+    "input_year, expected",
+    [
+        ([2021, 2024], [2022, 2023, 2024]),
+    ],
+)
+def test_build_years_minimum(input_year, expected):
+    """
+    Test that minimum years are filtered out
+    """
+    test = build_years_list(input_year, min_year=2022)
+    assert test == expected
+
+
+@pytest.mark.parametrize(
+    "input_year",
+    [3100, 2010, [2010, 2012]],
+)
+def test_ftn_charting_file_list_invalid(input_year):
+    """
+    Test that an input that contains all years less than minimum
+    available year raises an error
+    """
+    with pytest.raises(ValueError):  # Expect ValueError to be raised
+        ftn_charting_file_list(input_year)
+
+
+@pytest.mark.parametrize(
+    "input_year, expected_len",
+    [(2023, 1), ([2021, 2024], 3)],
+)
+def test_ftn_charting_file_list(input_year, expected_len):
+    """Test FTN charting creates lists after 2022"""
+    test = ftn_charting_file_list(input_year)
+    assert len(test) == expected_len
+    assert all(isinstance(x, str) for x in test)
